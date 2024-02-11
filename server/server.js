589df7a6dbbc417feb78c18d4a8febb12c5abfc8
Kelly Manson <klmanson@student.fullsail.edu>
2024-02-11 12:36:21 -0700

    removed null files

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const port = process.env.PORT;
 const cors = require('cors'); 
+const path = require('path'); 
 const app = express();
 const mongoose = require("mongoose");
 
 
-// allow cross-origin 
 app.use(cors()); 
+app.use(express.json());
 
 // server 
 app.listen(port, () => {
@@ -21,3 +22,12 @@ mongoose.connect(DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Database Connection Established"));
+
+// look in the react build folder for static build
+app.use(express.static(path.join(__dirname, "../reactjs/build")));
+
+// for any routes not defined by the api, assume it's a direct
+// request to a client-side route
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../reactjs/build", "index.html"));
+});
